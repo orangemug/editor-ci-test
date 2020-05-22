fa89617e1b676ae787721f7531f3c8efdfa6c771
orangemug <info@orangemug.co.uk>
2020-05-22 14:09:14 +0100

    Added back in some tests.

--- test/functional/index.js
+++ test/functional/index.js
@@ -15,30 +15,30 @@ describe('maputnik', function() {
     helper.stopGeoserver(done);
   });
 
-  // beforeEach(function() {
-  //   browser.url(config.baseUrl+"?debug&style="+helper.getStyleUrl([
-  //     "geojson:example",
-  //     "raster:raster"
-  //   ]));
-  //   browser.acceptAlert();
-  //   browser.execute(function() {
-  //     localStorage.setItem("survey", true);
-  //   });
-  //   const elem = $(".maputnik-toolbar-link");
-  //   elem.waitForExist();
-  //   browser.flushReactUpdates();
-  // });
+  beforeEach(function() {
+    browser.url(config.baseUrl+"?debug&style="+helper.getStyleUrl([
+      "geojson:example",
+      "raster:raster"
+    ]));
+    browser.acceptAlert();
+    browser.execute(function() {
+      localStorage.setItem("survey", true);
+    });
+    const elem = $(".maputnik-toolbar-link");
+    elem.waitForExist();
+    browser.flushReactUpdates();
+  });
 
-  // // -------- setup --------
-  // require("./util/coverage");
-  // // -----------------------
+  // -------- setup --------
+  require("./util/coverage");
+  // -----------------------
 
   // // ---- All the tests ----
   // require("./history");
   // require("./layers");
   // require("./map");
   // require("./modals");
-  // require("./screenshots");
+  require("./screenshots");
   // // ------------------------
 
 });
